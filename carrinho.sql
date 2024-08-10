244c49abe2d5f088ab87839fd5da1d3ea3018131
ArthurMLi <12200069@aluno.cotemig.com.br>
2024-08-09 23:59:39 -0300

    carrinho funcional

--- carrinho.sql
+++ carrinho.sql
@@ -1,125 +1,164 @@
-use classicmodels;
-create table carrinho(
-CODIGOCLIENTE INT NOT NULL,
-CODIGOPRODUTO VARCHAR(10),
-QUANTIDADE INT,
-PRECO DECIMAL(10,2),
-PRIMARY KEY (CODIGOCLIENTE, CODIGOPRODUTO) ) ENGINE=INNODB;
+USE classicmodels;
+
+CREATE TABLE carrinho (
+    CODIGOCLIENTE INT NOT NULL,
+    CODIGOPRODUTO VARCHAR(10),
+    QUANTIDADE INT,
+    PRECO DECIMAL(10,2),
+    PRIMARY KEY (CODIGOCLIENTE, CODIGOPRODUTO)
+) ENGINE=INNODB;
 
 DELIMITER $
-create procedure GERAR_ITEM_PEDIDO (IN PARAM_CODIGOPRODUTO INT, IN PARAM_QUANTIDADE INT, IN PARAM_PRECO INT, IN PARAM_NUMEROPEDIDO INT, IN PARAM_ORDERLINEBNUMBER INT, OUT ERRO VARCHAR(100))
-INICIO: BEGIN 
-declare produto_existente int default 1;
-    SELECT ifnull(PRODUCTCODE, 0) INTO produto_existente FROM PRODUCTS WHERE PRODUCTCODE = PARAM_CODIGOPRODUTO;
-	IF produto_existente = 0 THEN
-	SET ERRO = "Produto invalido";
-        leave INICIO;
+
+CREATE PROCEDURE GERAR_ITEM_PEDIDO (IN PARAM_CODIGOPRODUTO VARCHAR(10), IN PARAM_QUANTIDADE INT, IN PARAM_PRECO DECIMAL(10,2), IN PARAM_NUMEROPEDIDO INT, IN PARAM_ORDERLINEBNUMBER INT, OUT ERRO VARCHAR(100))
+inicio: BEGIN 
+    DECLARE PRODUTO_EXISTENTE INT;
+    DECLARE QUANTIDADE_ESTOQUE INT;
+
+    -- Verifica se o produto existe e obter a quantidade em estoque
+    SELECT COUNT(*), IFNULL(QUANTITYINSTOCK, 0) INTO PRODUTO_EXISTENTE, QUANTIDADE_ESTOQUE
+    FROM PRODUCTS 
+    WHERE PRODUCTCODE = PARAM_CODIGOPRODUTO;
+
+    -- Verifica se o produto foi encontrado
+    IF PRODUTO_EXISTENTE = 0 THEN
+        SET ERRO = 'Produto inválido';
+        LEAVE inicio;
     END IF;
-    IF (SELECT QUANTITYINSTOCK FROM PRODUCTS WHERE PRODUCTCODE = PARAM_CODIGOPRODUTO) < PARAM_QUANTIDADE THEN
-		SET ERRO = concat("Quantidade do produto",  PARAM_CODIGOPRODUTO , "invalida");
-		leave INICIO;
-    END if;
+
+    -- Verifica se a quantidade em estoque é suficiente
+    IF QUANTIDADE_ESTOQUE < PARAM_QUANTIDADE THEN
+        SET ERRO = CONCAT('Quantidade do produto ', PARAM_CODIGOPRODUTO, ' inválida');
+        LEAVE inicio;
+    END IF;
+
+    -- Inseri o item no pedido
+    INSERT INTO ORDERDETAILS 
+        VALUES (PARAM_NUMEROPEDIDO, PARAM_CODIGOPRODUTO, PARAM_QUANTIDADE, PARAM_PRECO, PARAM_ORDERLINEBNUMBER);
     
-	INSERT INTO ORDERDETAILS 
-		VALUES (PARAM_NUMEROPEDIDO, PARAM_CODIGOPRODUTO, PARAM_QUANTIDADE, PARAM_PRECO, PARAM_ORDERLINEBNUMBER);
-	SET ERRO = "";
-	
-    update products set quantityinstock = (quantityinstock - param_quantidade) where productCode = param_codigoproduto;
+    -- Atualizar quantidade em estoque
+    UPDATE PRODUCTS 
+    SET QUANTITYINSTOCK = QUANTITYINSTOCK - PARAM_QUANTIDADE 
+    WHERE PRODUCTCODE = PARAM_CODIGOPRODUTO;
+    
+    SET ERRO = '';
 END$
 
 CREATE PROCEDURE GERAR_PEDIDO (IN PARAM_CLIENTE INT, IN PARAM_VENDEDOR INT, OUT RESULTADO VARCHAR(200))
-INICIO: BEGIN
-	#Variaveis para conferencia
+inicio: BEGIN
     DECLARE VAR_EXISTECLIENTE INT DEFAULT 0;
-	DECLARE VAR_EXISTEVENDEDOR INT DEFAULT 0;
-	DECLARE VAR_COMPROUPRODUTO INT DEFAULT 0;
-    #Variaveis do carrinho
+    DECLARE VAR_EXISTEVENDEDOR INT DEFAULT 0;
+    DECLARE VAR_COMPROUPRODUTO INT DEFAULT 0;
     DECLARE VAR_NUMEROPEDIDO INT DEFAULT 0;
-    DECLARE VAR_CODIGOPRODUTO INT DEFAULT 0;
-    DECLARE VAR_QUANTIDADE INT DEFAULT 0;
-    DECLARE VAR_PRECO INT DEFAULT 0;
-    
-    #Variaveis para o while
-	DECLARE cont INT DEFAULT 0;
-	declare contador_while int default 0;
+    DECLARE VAR_CODIGOPRODUTO VARCHAR(10);
+    DECLARE VAR_QUANTIDADE INT;
+    DECLARE VAR_PRECO DECIMAL(10,2);
+    DECLARE CONT INT DEFAULT 0;
+    DECLARE CONTADOR_WHILE INT DEFAULT 0;
+    DECLARE ERRO VARCHAR(100);
 
-	
-    SELECT MAX(ORDERNUMBER) + 1 INTO VAR_NUMEROPEDIDO FROM ORDERS;
-    
-  # Verifica a existencia do cliente
-	SELECT IFNULL(CUSTOMERNUMBER,0) INTO VAR_EXISTECLIENTE
-	FROM CUSTOMERS
-	WHERE CUSTOMERNUMBER = PARAM_CLIENTE;
+    -- Inicia transação
+    START TRANSACTION;
 
-	IF VAR_EXISTECLIENTE = 0 THEN
-		SET RESULTADO = "CLIENTE NÃO ENCONTRADO NA BASE DE DADOS";
-		LEAVE INICIO;
-	END IF;
-    
-  # Verifica a existencia do vendedor
-  SELECT IFNULL(employeeNUMBER,0) INTO VAR_EXISTEvendedor
-	FROM employees
-	WHERE employeeNUMBER = PARAM_vendedor;
-    IF VAR_EXISTEVENDEDOR = 0 THEN
-		SET RESULTADO = "VENDEDOR NÃO ENCONTRADO NA BASE DE DADOS";
-		LEAVE INICIO;
-	END IF;
-    
-  # Verifica se o carrinho ja foi comprado
-    
-	SELECT IFNULL(COUNT(*), 0) INTO VAR_COMPROUPRODUTO
-	FROM CARRINHO
-	WHERE CODIGOCLIENTE = PARAM_CLIENTE;
+    -- Obtem o próximo número do pedido
+    SELECT IFNULL(MAX(ORDERNUMBER), 0) + 1 INTO VAR_NUMEROPEDIDO FROM ORDERS;
 
-	IF VAR_COMPROUPRODUTO = 0 THEN
-		SET RESULTADO = 'O CARRINHO ESTÁ VAZIO';
-		LEAVE INICIO;
-	END IF;
-	
-  # Verifica o limite de credito do cliente
-    IF (select creditLimit from customers where	customerNumber = param_cliente) < 0 then
-		SET RESULTADO = 'O CLIENTE NÃO POSSUI LIMITE DE CREDITO';
-		LEAVE INICIO;
+    -- Verifica a existência do cliente
+    SELECT COUNT(*) INTO VAR_EXISTECLIENTE
+    FROM CUSTOMERS
+    WHERE CUSTOMERNUMBER = PARAM_CLIENTE;
+
+    IF VAR_EXISTECLIENTE = 0 THEN
+        SET RESULTADO = 'CLIENTE NÃO ENCONTRADO NA BASE DE DADOS';
+        ROLLBACK;
+        LEAVE inicio;
     END IF;
-   
-# Conclui a ordem de pedido
-     START TRANSACTION;
-    INSERT INTO ORDERS (orderNumber, orderdate, requiredDate, shippeddate, status, comments, customernumber)
-		VALUES (VAR_NUMEROPEDIDO, CURDATE(), CURDATE() + 7, '', 'processing','', 'param_cliente');
+
+    -- Verifica a existência do vendedor
+    SELECT COUNT(*) INTO VAR_EXISTEVENDEDOR
+    FROM EMPLOYEES
+    WHERE EMPLOYEENUMBER = PARAM_VENDEDOR;
+
+    IF VAR_EXISTEVENDEDOR = 0 THEN
+        SET RESULTADO = 'VENDEDOR NÃO ENCONTRADO NA BASE DE DADOS';
+        ROLLBACK;
+        LEAVE inicio;
+    END IF;
+
+    -- Verifica se o carrinho está vazio
+    SELECT COUNT(*) INTO VAR_COMPROUPRODUTO
+    FROM CARRINHO
+    WHERE CODIGOCLIENTE = PARAM_CLIENTE;
+
+    IF VAR_COMPROUPRODUTO = 0 THEN
+        SET RESULTADO = 'O CARRINHO ESTÁ VAZIO';
+        ROLLBACK;
+        LEAVE inicio;
+    END IF;
+
+    -- Verifica o limite de crédito do cliente
+    IF (SELECT CREDITLIMIT FROM CUSTOMERS WHERE CUSTOMERNUMBER = PARAM_CLIENTE) < 0 THEN
+        SET RESULTADO = 'O CLIENTE NÃO POSSUI LIMITE DE CRÉDITO';
+        ROLLBACK;
+        LEAVE inicio;
+    END IF;
+
+    -- Inseri o pedido
+    INSERT INTO ORDERS (ORDERNUMBER, ORDERDATE, REQUIREDDATE, SHIPPEDDATE, STATUS, COMMENTS, CUSTOMERNUMBER)
+    VALUES (VAR_NUMEROPEDIDO, CURDATE(), CURDATE() + INTERVAL 7 DAY, NULL, 'processing', '', PARAM_CLIENTE);
+
+    -- Obtem o número total de itens no carrinho
+    SELECT COUNT(*) INTO CONT FROM CARRINHO WHERE CODIGOCLIENTE = PARAM_CLIENTE;
+
+    -- Processa cada item do carrinho
+    WHILE CONTADOR_WHILE < CONT DO
+        SET ERRO = '';
+
+        -- Obte detalhes do item no carrinho
+        SELECT CODIGOPRODUTO, QUANTIDADE, PRECO INTO VAR_CODIGOPRODUTO, VAR_QUANTIDADE, VAR_PRECO
+        FROM CARRINHO 
+        WHERE CODIGOCLIENTE = PARAM_CLIENTE 
+        LIMIT 1 OFFSET CONTADOR_WHILE;
         
-# Adiciona os itens
-	
-    select sum(quantidade) into cont from carrinho where codigocliente = param_cliente;
-    
-    WHILE contador_while <= cont do
-    set @erro = "";
-    SELECT CODIGOPRODUTO INTO VAR_CODIGOPRODUTO FROM CARRINHO WHERE CODIGOCLIENTE = PARAM_CLIENTE LIMIT 1 offset CONTADOR_WHILE;
-	SELECT QUANTIDADE INTO VAR_QUANTIDADE FROM CARRINHO WHERE CODIGOCLIENTE = PARAM_CLIENTE LIMIT 1 offset CONTADOR_WHILE;
-    SELECT PRECO INTO VAR_PRECO FROM CARRINHO WHERE CODIGOCLIENTE = PARAM_CLIENTE LIMIT 1 offset CONTADOR_WHILE;
-    
-    CALL GERAR_ITEM_PEDIDO (VAR_CODIGOPRODUTO, VAR_QUANTIDADE , VAR_PRECO, VAR_NUMEROPEDIDO, CONTADOR_WHILE + 1, @erro);
-		if (@erro != "") then
-            rollback;
-			set resultado = @erro;
-            leave inicio;
-		end if;
-	set contador_while = contador_while + 1;
+        -- Gera item do pedido
+        CALL GERAR_ITEM_PEDIDO(VAR_CODIGOPRODUTO, VAR_QUANTIDADE, VAR_PRECO, VAR_NUMEROPEDIDO, CONTADOR_WHILE + 1, ERRO);
+        
+        -- Verifica se houve erro
+        IF ERRO != '' THEN
+            ROLLBACK;
+            SET RESULTADO = ERRO;
+            LEAVE inicio;
+        END IF;
+
+        SET CONTADOR_WHILE = CONTADOR_WHILE + 1;
     END WHILE;
-    update customers set salesRepEmployeeNumber = PARAM_VENDEDOR where customernumber = PARAM_cliente;
-    set resultado  =  concat("pedido gerado: ",VAR_NUMEROPEDIDO);
-    delete from carrinho where CODIGOCLIENTE = param_cliente;
+
+    -- Atualiza vendedor
+    UPDATE CUSTOMERS SET SALESREP_EMPLOYEENUMBER = PARAM_VENDEDOR WHERE CUSTOMERNUMBER = PARAM_CLIENTE;
+
+    -- Confirma a transação
     COMMIT;
-    ROLLBACK;
-    
+
+    SET RESULTADO = CONCAT('Pedido gerado: ', VAR_NUMEROPEDIDO);
+
+    -- Limpa o carrinho
+    DELETE FROM CARRINHO WHERE CODIGOCLIENTE = PARAM_CLIENTE;
+
 END$
 DELIMITER ;
-insert into carrinho values(103,"aASDASDAS",10,10);
-call GERAR_PEDIDO(103,1002,@resultado);
-insert into resultado values(@resultado);
-create table resultado(
-id varchar(100)
+
+-- Testa o procedimento
+INSERT INTO CARRINHO VALUES (103, 'a', 10, 10);
+CALL GERAR_PEDIDO(103, 1002, @resultado);
+
+-- Cria tabela para armazenar resultados
+CREATE TABLE resultado (
+    id VARCHAR(100)
 );
-select * from resultado;
-select * from carrinho;
+
+-- Inseri e verificar o resultado
+INSERT INTO resultado VALUES (@resultado);
+SELECT * FROM resultado;
+SELECT * FROM CARRINHO;
 
 
